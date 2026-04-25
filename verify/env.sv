817f4a8293b68c5adfb90bcbbf6620def64bbbcf
siwiaszczykb <siwiaszczykb@gmail.com>
2026-04-25 15:01:07 +0200

    final commit for today I guess, basic sequencer definition

--- verify/env.sv
+++ verify/env.sv
@@ -1,2 +1,7 @@
 import tb_pkg::*;
 
+class i2c_env extends uvm_env;
+
+
+
+endclass
--- verify/sequencer.sv
+++ verify/sequencer.sv
@@ -1,1 +1,9 @@
 import tb_pkg::*;
+
+class i2c_sequencer extends uvm_sequencer;
+	`uvm_component_utils (i2c_sequencer)
+	function new (string name="i2c_sequencer", uvm_component parent);
+		super.new (name, parent);
+	endfunction
+
+endclass
