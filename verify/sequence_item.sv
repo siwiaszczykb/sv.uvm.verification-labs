0c150d17e2b7b814308520977219feba4c4582ab
siwiaszczykb <siwiaszczykb@gmail.com>
2026-04-25 13:28:32 +0200

    sequence item implementation's attempt

--- verify/sequence_item.sv
+++ verify/sequence_item.sv
@@ -1,1 +1,26 @@
 import tb_pkg::*;
+
+class i2c_seq_item extends uvm_sequence_item;
+
+rand bit [16:0]     addr;
+rand bit            wr_en;
+rand bit            rd_en;
+rand cmd_t          cmd;
+rand bit [7:0]      w_data;    
+bit      [23:0]     r_data;
+bit                 r_data_valid;
+bit                 ready;
+
+`uvm_object_utils_begin(i2c_seq_item)
+    `uvm_field_int(addr,UVM_ALL_ON)
+    `uvm_field_int(wr_en,UVM_ALL_ON)
+    `uvm_field_int(rd_en,UVM_ALL_ON)
+    `uvm_field_int(w_data,UVM_ALL_ON)
+    `uvm_field_int(r_data,UVM_ALL_ON)
+`uvm_object_utils_end
+
+function new(string name = "i2c_seq_item");
+    super.new(name);
+endfunction
+
+endclass
